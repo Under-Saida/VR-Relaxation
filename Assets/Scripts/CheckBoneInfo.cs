5fedf3a12efc8f427ad2fdfe278cb06776f50d82
UnderSaida- <under.saida@gmail.com>
2021-12-11 14:23:40 +0900

    fix scripts
    「CheckBoneInfo.cs」と「EEGdata」を修正し、脳波などのデータをtxtファイルに出力できるようにした。

--- Assets/Scripts/CheckBoneInfo.cs
+++ Assets/Scripts/CheckBoneInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using System.IO;
 
 public class CheckBoneInfo : MonoBehaviour
 {
@@ -25,20 +27,21 @@ public class CheckBoneInfo : MonoBehaviour
 
     bool CheckBonePostionDifference() 
     {
-        if (HeadPos_diff.x < 0.05 && HeadPos_diff.y < 0.05 && HeadPos_diff.z < 0.05 &&
-        NeckPos_diff.x < 0.05 && NeckPos_diff.y < 0.05 && NeckPos_diff.z < 0.05 &&
+        // 差分の絶対値が0.05以下なら、動きが合っているとする。
+        if (Mathf.Abs(HeadPos_diff.x) < 0.05 && Mathf.Abs(HeadPos_diff.y) < 0.05 && Mathf.Abs(HeadPos_diff.z) < 0.05 && 
+            Mathf.Abs(NeckPos_diff.x) < 0.05 && Mathf.Abs(NeckPos_diff.y) < 0.05 && Mathf.Abs(NeckPos_diff.z) < 0.05 &&
 
-        LeftShoulderPos_diff.x < 0.05 && LeftShoulderPos_diff.y < 0.05 && LeftShoulderPos_diff.z < 0.05 &&
-        RightShoulderPos_diff.x < 0.05 && RightShoulderPos_diff.y < 0.05 && RightShoulderPos_diff.z < 0.05 &&
+            Mathf.Abs(LeftShoulderPos_diff.x) < 0.05 && Mathf.Abs(LeftShoulderPos_diff.y) < 0.05 && Mathf.Abs(LeftShoulderPos_diff.z) < 0.05 &&
+            Mathf.Abs(RightShoulderPos_diff.x) < 0.05 && Mathf.Abs(RightShoulderPos_diff.y) < 0.05 && Mathf.Abs(RightShoulderPos_diff.z) < 0.05 &&
 
-        LeftUpperArmPos_diff.x < 0.05 && LeftUpperArmPos_diff.y < 0.05 && LeftUpperArmPos_diff.z < 0.05 &&
-        RightUpperArmPos_diff.x < 0.05 && RightUpperArmPos_diff.y < 0.05 && RightUpperArmPos_diff.z < 0.05 &&
+            Mathf.Abs(LeftUpperArmPos_diff.x) < 0.05 && Mathf.Abs(LeftUpperArmPos_diff.y) < 0.05 && Mathf.Abs(LeftUpperArmPos_diff.z) < 0.05 &&
+            Mathf.Abs(RightUpperArmPos_diff.x) < 0.05 && Mathf.Abs(RightUpperArmPos_diff.y) < 0.05 && Mathf.Abs(RightUpperArmPos_diff.z) < 0.05 &&
 
-        LeftLowerArmPos_diff.x < 0.05 && LeftLowerArmPos_diff.y < 0.05 && LeftLowerArmPos_diff.z < 0.05 &&
-        LeftLowerArmPos_diff.x < 0.05 && LeftLowerArmPos_diff.y < 0.05 && LeftLowerArmPos_diff.z < 0.05)
+            Mathf.Abs(LeftLowerArmPos_diff.x) < 0.05 && Mathf.Abs(LeftLowerArmPos_diff.y) < 0.05 && Mathf.Abs(LeftLowerArmPos_diff.z) < 0.05 &&
+            Mathf.Abs(LeftLowerArmPos_diff.x) < 0.05 && Mathf.Abs(LeftLowerArmPos_diff.x) < 0.05 && Mathf.Abs(LeftLowerArmPos_diff.x) < 0.05 &&
 
-        //LeftHandPos_diff.x < 0.05 && LeftHandPos_diff.y < 0.05 && LeftHandPos_diff.z < 0.05 &&
-        //RightHandPos_diff.x < 0.05 && RightHandPos_diff.y < 0.05 && RightHandPos_diff.z < 0.05)
+            Mathf.Abs(LeftHandPos_diff.x) < 0.05 && Mathf.Abs(LeftHandPos_diff.y) < 0.05 && Mathf.Abs(LeftHandPos_diff.z) < 0.05 &&
+            Mathf.Abs(RightHandPos_diff.x) < 0.05 && Mathf.Abs(RightHandPos_diff.y) < 0.05 && Mathf.Abs(RightHandPos_diff.z) < 0.05)
         {
             bonePosCheck_ok = true;
             return bonePosCheck_ok;
@@ -52,20 +55,21 @@ public class CheckBoneInfo : MonoBehaviour
 
     bool CheckBoneRotationDifference()
     {
-        if (HeadRot_diff.x < 0.05 && HeadRot_diff.y < 0.05 && HeadRot_diff.z < 0.05 &&
-        NeckRot_diff.x < 0.05 && NeckRot_diff.y < 0.05 && NeckRot_diff.z < 0.05 &&
+        // 差分の絶対値が0.05以下なら、動きが合っているとする。
+        if (Mathf.Abs(HeadRot_diff.x) < 0.05 && Mathf.Abs(HeadRot_diff.y) < 0.05 && Mathf.Abs(HeadRot_diff.z) < 0.05 &&
+            Mathf.Abs(NeckRot_diff.x) < 0.05 && Mathf.Abs(NeckRot_diff.y) < 0.05 && Mathf.Abs(NeckRot_diff.z) < 0.05 &&
 
-        LeftShoulderRot_diff.x < 0.05 && LeftShoulderRot_diff.y < 0.05 && LeftShoulderRot_diff.z < 0.05 &&
-        RightShoulderRot_diff.x < 0.05 && RightShoulderRot_diff.y < 0.05 && RightShoulderRot_diff.z < 0.05 &&
+            Mathf.Abs(LeftShoulderRot_diff.x) < 0.05 && Mathf.Abs(LeftShoulderRot_diff.y) < 0.05 && Mathf.Abs(LeftShoulderRot_diff.z) < 0.05 &&
+            Mathf.Abs(RightShoulderRot_diff.x) < 0.05 && Mathf.Abs(RightShoulderRot_diff.y) < 0.05 && Mathf.Abs(RightShoulderRot_diff.z) < 0.05 &&
 
-        LeftUpperArmRot_diff.x < 0.05 && LeftUpperArmRot_diff.y < 0.05 && LeftUpperArmRot_diff.z < 0.05 &&
-        RightUpperArmRot_diff.x < 0.05 && RightUpperArmRot_diff.y < 0.05 && RightUpperArmRot_diff.z < 0.05 &&
+            Mathf.Abs(LeftUpperArmRot_diff.x) < 0.05 && Mathf.Abs(LeftUpperArmRot_diff.y) < 0.05 && Mathf.Abs(LeftUpperArmRot_diff.z) < 0.05 &&
+            Mathf.Abs(RightUpperArmRot_diff.x) < 0.05 && Mathf.Abs(RightUpperArmRot_diff.y) < 0.05 && Mathf.Abs(RightUpperArmRot_diff.z) < 0.05 &&
 
-        LeftLowerArmRot_diff.x < 0.05 && LeftLowerArmRot_diff.y < 0.05 && LeftLowerArmRot_diff.z < 0.05 &&
-        LeftLowerArmRot_diff.x < 0.05 && LeftLowerArmRot_diff.y < 0.05 && LeftLowerArmRot_diff.z < 0.05)
+            Mathf.Abs(LeftLowerArmRot_diff.x) < 0.05 && Mathf.Abs(LeftLowerArmRot_diff.y) < 0.05 && Mathf.Abs(LeftLowerArmRot_diff.z) < 0.05 &&
+            Mathf.Abs(LeftLowerArmRot_diff.x) < 0.05 && Mathf.Abs(LeftLowerArmRot_diff.x) < 0.05 && Mathf.Abs(LeftLowerArmRot_diff.x) < 0.05 &&
 
-        //LeftHandRot_diff.x < 0.05 && LeftHandRot_diff.y < 0.05 && LeftHandRot_diff.z < 0.05 &&
-        //RightHandRot_diff.x < 0.05 && RightHandRot_diff.y < 0.05 && RightHandRot_diff.z < 0.05)
+            Mathf.Abs(LeftHandRot_diff.x) < 0.05 && Mathf.Abs(LeftHandRot_diff.y) < 0.05 && Mathf.Abs(LeftHandRot_diff.z) < 0.05 &&
+            Mathf.Abs(RightHandRot_diff.x) < 0.05 && Mathf.Abs(RightHandRot_diff.y) < 0.05 && Mathf.Abs(RightHandRot_diff.z) < 0.05)
         {
             boneRotCheck_ok = true;
             return boneRotCheck_ok;
@@ -77,9 +81,165 @@ public class CheckBoneInfo : MonoBehaviour
         }
     }
 
+    void ExportCloneBoneInfo()
+    {
+        //cloneHead
+        StreamWriter clone_head = new StreamWriter("../data/CloneBone_data/Head.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_head.WriteLine(cloneHead);
+        clone_head.Close();
 
+        //cloneNeck
+        StreamWriter clone_neck = new StreamWriter("../data/CloneBone_data/Neck.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_neck.WriteLine(cloneNeck);
+        clone_neck.Close();
 
+        //cloneLeftShoulder
+        StreamWriter clone_L_shoulder = new StreamWriter("../data/CloneBone_data/LeftShoulder.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_L_shoulder.WriteLine(cloneLeftShoulder);
+        clone_L_shoulder.Close();
 
+        //cloneRightShoulder
+        StreamWriter clone_R_shoulder = new StreamWriter("../data/CloneBone_data/RightShoulder.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_R_shoulder.WriteLine(cloneRightShoulder);
+        clone_R_shoulder.Close();
+
+        //cloneLeftUpperArm
+        StreamWriter clone_L_upperArm = new StreamWriter("../data/CloneBone_data/LeftUpperArm.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_L_upperArm.WriteLine(cloneLeftUpperArm);
+        clone_L_upperArm.Close();
+
+        //cloneRightUpperArm
+        StreamWriter clone_R_upperArm = new StreamWriter("../data/CloneBone_data/RightUpperArm.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_R_upperArm.WriteLine(cloneRightUpperArm);
+        clone_R_upperArm.Close();
+
+        //cloneLeftLowerArm
+        StreamWriter clone_L_lowerArm = new StreamWriter("../data/CloneBone_data/LeftLowerArm.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_L_lowerArm.WriteLine(cloneLeftLowerArm);
+        clone_L_lowerArm.Close();
+
+        //cloneRightLowerArm
+        StreamWriter clone_R_lowerArm = new StreamWriter("../data/CloneBone_data/RightLowerArm.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_R_lowerArm.WriteLine(cloneRightLowerArm);
+        clone_R_lowerArm.Close();
+
+        //cloneLeftHand
+        StreamWriter clone_L_hand = new StreamWriter("../data/CloneBone_data/LeftHand.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_L_hand.WriteLine(cloneLeftHand);
+        clone_L_hand.Close();
+
+        //cloneRightHand
+        StreamWriter clone_R_hand = new StreamWriter("../data/CloneBone_data/RightHand.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        clone_R_hand.WriteLine(cloneRightHand);
+        clone_R_hand.Close();
+
+    }
+
+    void ExportBonePosition_diff()
+    {
+        //HeadPos_diff
+        StreamWriter headPos_diff = new StreamWriter("../data/BonePosition_diff/HeadPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        headPos_diff.WriteLine(HeadPos_diff);
+        headPos_diff.Close();
+
+        //NeckPos_diff
+        StreamWriter neckPos_diff = new StreamWriter("../data/BonePosition_diff/NeckPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        neckPos_diff.WriteLine(NeckPos_diff);
+        neckPos_diff.Close();
+
+        //LeftShoulderPos_diff
+        StreamWriter L_shoulderPos_diff = new StreamWriter("../data/BonePosition_diff/LeftShoulderPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_shoulderPos_diff.WriteLine(LeftShoulderPos_diff);
+        L_shoulderPos_diff.Close();
+
+        //RightShoulderPos_diff
+        StreamWriter R_shoulder_diff = new StreamWriter("../data/BonePosition_diff/RightShoulderPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_shoulder_diff.WriteLine(RightShoulderPos_diff);
+        R_shoulder_diff.Close();
+
+        //LeftUpperArmPos_diff
+        StreamWriter L_upperArmPos_diff = new StreamWriter("../data/BonePosition_diff/LeftUpperArmPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_upperArmPos_diff.WriteLine(LeftUpperArmPos_diff);
+        L_upperArmPos_diff.Close();
+
+        //RightUpperArmPos_diff
+        StreamWriter R_upperArmPos_diff = new StreamWriter("../data/BonePosition_diff/RightUpperArmPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_upperArmPos_diff.WriteLine(RightUpperArmPos_diff);
+        R_upperArmPos_diff.Close();
+
+        //LeftLowerArmPos_diff
+        StreamWriter L_lowerArmPos_diff = new StreamWriter("../data/BonePosition_diff/LeftLowerArmPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_lowerArmPos_diff.WriteLine(LeftLowerArmPos_diff);
+        L_lowerArmPos_diff.Close();
+
+        //RightLowerArmPos_diff
+        StreamWriter R_lowerArmPos_diff = new StreamWriter("../data/BonePosition_diff/RightLowerArmPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_lowerArmPos_diff.WriteLine(RightLowerArmPos_diff);
+        R_lowerArmPos_diff.Close();
+
+        //LeftHandPos_diff
+        StreamWriter L_handPos_diff = new StreamWriter("../data/BonePosition_diff/LeftHandPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_handPos_diff.WriteLine(LeftHandPos_diff);
+        L_handPos_diff.Close();
+
+        //RightHandPos_diff
+        StreamWriter R_handPos_diff = new StreamWriter("../data/BonePosition_diff/RightHandPos_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_handPos_diff.WriteLine(RightHandPos_diff);
+        R_handPos_diff.Close();
+    }
+
+    void ExportBoneRotation_diff()
+    {
+        //HeadRot_diff
+        StreamWriter headRot_diff = new StreamWriter("../data/BoneRotation_diff/HeadRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        headRot_diff.WriteLine(HeadRot_diff);
+        headRot_diff.Close();
+
+        //NeckRot_diff
+        StreamWriter neckRot_diff = new StreamWriter("../data/BoneRotation_diff/NeckRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        neckRot_diff.WriteLine(NeckRot_diff);
+        neckRot_diff.Close();
+
+        //LeftShoulderRot_diff
+        StreamWriter L_shoulderRot_diff = new StreamWriter("../data/BoneRotation_diff/LeftShoulderRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_shoulderRot_diff.WriteLine(LeftShoulderRot_diff);
+        L_shoulderRot_diff.Close();
+
+        //RightShoulderRot_diff
+        StreamWriter R_shoulder_diff = new StreamWriter("../data/BoneRotation_diff/RightShoulderRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_shoulder_diff.WriteLine(RightShoulderRot_diff);
+        R_shoulder_diff.Close();
+
+        //LeftUpperArmRot_diff
+        StreamWriter L_upperArmRot_diff = new StreamWriter("../data/BoneRotation_diff/LeftUpperArmRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_upperArmRot_diff.WriteLine(LeftUpperArmRot_diff);
+        L_upperArmRot_diff.Close();
+
+        //RightUpperArmRot_diff
+        StreamWriter R_upperArmRot_diff = new StreamWriter("../data/BoneRotation_diff/RightUpperArmRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_upperArmRot_diff.WriteLine(RightUpperArmRot_diff);
+        R_upperArmRot_diff.Close();
+
+        //LeftLowerArmRot_diff
+        StreamWriter L_lowerArmRot_diff = new StreamWriter("../data/BoneRotation_diff/LeftLowerArmRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_lowerArmRot_diff.WriteLine(LeftLowerArmRot_diff);
+        L_lowerArmRot_diff.Close();
+
+        //RightLowerArmRot_diff
+        StreamWriter R_lowerArmRot_diff = new StreamWriter("../data/BoneRotation_diff/RightLowerArmRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_lowerArmRot_diff.WriteLine(RightLowerArmRot_diff);
+        R_lowerArmRot_diff.Close();
+
+        //LeftHandRot_diff
+        StreamWriter L_handRot_diff = new StreamWriter("../data/BoneRotation_diff/LeftHandRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        L_handRot_diff.WriteLine(LeftHandRot_diff);
+        L_handRot_diff.Close();
+
+        //RightHandRot_diff
+        StreamWriter R_handRot_diff = new StreamWriter("../data/BoneRotation_diff/RightHandRot_diff.txt", true, Encoding.GetEncoding("Shift_JIS"));
+        R_handRot_diff.WriteLine(RightHandRot_diff);
+        R_handRot_diff.Close();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -141,6 +301,7 @@ public class CheckBoneInfo : MonoBehaviour
         cloneRightHand = clone_boneinfo.transRightHand;
 
 
+
         // ボーンPosition情報の差分を部位ごとに格納(Vector3に変換)
         HeadPos_diff = clone_boneinfo.transHead.position - my_boneinfo.transHead.position;
 
@@ -158,9 +319,9 @@ public class CheckBoneInfo : MonoBehaviour
 
         RightLowerArmPos_diff = clone_boneinfo.transRightLowerArm.position - my_boneinfo.transRightLowerArm.position;
 
-        //LeftHandPos_diff = clone_boneinfo.transLeftHand.position - my_boneinfo.transLeftHand.position;
+        LeftHandPos_diff = clone_boneinfo.transLeftHand.position - my_boneinfo.transLeftHand.position;
 
-        //RightHandPos_diff = clone_boneinfo.transRightHand.position - my_boneinfo.transRightHand.position;
+        RightHandPos_diff = clone_boneinfo.transRightHand.position - my_boneinfo.transRightHand.position;
 
 
 
@@ -181,9 +342,14 @@ public class CheckBoneInfo : MonoBehaviour
 
         RightLowerArmRot_diff = clone_boneinfo.transRightLowerArm.rotation.eulerAngles - my_boneinfo.transRightLowerArm.rotation.eulerAngles;
 
-        //LeftHandRot_diff = clone_boneinfo.transLeftHand.rotation.eulerAngles - my_boneinfo.transLeftHand.rotation.eulerAngles;
+        LeftHandRot_diff = clone_boneinfo.transLeftHand.rotation.eulerAngles - my_boneinfo.transLeftHand.rotation.eulerAngles;
 
-        //RightHandRot_diff = clone_boneinfo.transRightHand.rotation.eulerAngles - my_boneinfo.transRightHand.rotation.eulerAngles;
+        RightHandRot_diff = clone_boneinfo.transRightHand.rotation.eulerAngles - my_boneinfo.transRightHand.rotation.eulerAngles;
+
+        // Bone情報の取得に使用（普段はコメントアウトしておく）
+        ExportCloneBoneInfo();
+        ExportBonePosition_diff();
+        ExportBoneRotation_diff();
 
 
         // 各ボーンのPositionの差分が一定以下であればTrueを返す。
