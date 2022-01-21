47f138ec124c87f85b3bed865f5b3efac9a77d13
UnderSaida- <under.saida@gmail.com>
2022-01-21 12:25:44 +0900

    add CheckInputInfo.cs
    CheckInputInfo.csの追加

---
+++ Assets/Scripts/CheckInputInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckInputInfo : MonoBehaviour
+{
+    PMR_AnimationController pmr_animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*　「記録を行う入力の条件など」
+     *  脳波の計測開始のタイミング
+     *  PMRの段階(腕 or 肩) + Hold状態か、Relax状態か
+     *  コントローラーの入力がされたとき(InputGetKeyDown)、コントローラーの入力が解かれた時(InputGetKeyUp)
+     */
+}
